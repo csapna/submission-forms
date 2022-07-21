60ad186007eb79e82b890cd03b2053a34e742115
Sapna <c.sapna@gmail.com>
2022-07-21 16:11:04 -0400

    Update signup.php

--- special-cases/answer-based-rating/signup.php
+++ special-cases/answer-based-rating/signup.php
@@ -8,16 +8,16 @@ $projectId = '1001';
 $apiKey = '1x1x1';
 
 if (empty($clientId) || empty($projectId) || empty($apiKey)){
-  throw new Exception('Required parameters are not set, please check that 
-                        your $clientId, $projectId and $apiKey
-                        are configured correctly');
+	throw new Exception('Required parameters are not set, please check that
+				your $clientId, $projectId and $apiKey
+				are configured correctly');
 }
 
 $lead = new LassoLead(
-  $_REQUEST['FirstName'],
-  $_REQUEST['LastName'],
-  $projectId,
-  $clientId
+	$_REQUEST['FirstName'],
+	$_REQUEST['LastName'],
+	$projectId,
+	$clientId
 );
 
 $lead->addPhone($_REQUEST['Phone']);
@@ -35,10 +35,10 @@ $lead->addEmail($_REQUEST['Email']);
  * }
  */
 if (array_key_exists('1111',$_POST['Questions']) && $_POST['Questions']['1111']=='101') {
-  $lead->setRating('R');
+	$lead->setRating('R');
 } 
 else {
-  $lead->setRating('N');
+	$lead->setRating('N');
 }
 
 /* Questions (select answer)
