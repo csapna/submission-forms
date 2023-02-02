40b3e45b225a70a193332e0976bf2b4aa6953b10
Sapna <c.sapna@gmail.com>
2023-02-02 16:05:51 -0500

    Add files via upload

---
+++ php-forms/php/multi-project.php
@@ -0,0 +1,43 @@
+<?php
+
+require('src/LassoLead.php');
+require('src/RegistrantSubmitter.php');
+
+$clientId  = '1111';
+$apiKey = '1x1x1';
+
+if (empty($clientId) || empty($_REQUEST['ProjectID']) || empty($apiKey)){
+	throw new Exception('Required parameters are not set, please check that
+							your $clientId, $projectId and $apiKey are
+							configured correctly');
+}
+
+$lead = new LassoLead(
+	$_REQUEST['FirstName'],
+    $_REQUEST['LastName'],
+    $_REQUEST['ProjectID'][0],
+    $clientId
+);
+
+/* Projects
+ * 
+ * For a single form to submit to multiple selected projects
+ */
+foreach($_REQUEST['ProjectID'] as $index => $projectId){
+	if ($index == 0){
+		continue;
+	}
+	$lead->addProject($projectId);
+}
+
+$lead->addPhone($_REQUEST['Phone']);
+
+$lead->addEmail($_REQUEST['Email']);
+
+$lead->signupEmailLink = 'http://www.website.com/thankyou-email.html';
+$lead->signupEmailSubject = 'Thank you for registering at [@PROJECT_NAME]';
+
+$lead->sendAssignmentNotification();
+
+$submitter = new RegistrantSubmitter();
+$curl      = $submitter->submit('https://api.lassocrm.com/registrants', $lead, $apiKey);
---
+++ php-forms/php/utm-answers.php
@@ -0,0 +1,42 @@
+<?php
+
+require('src/LassoLead.php');
+require('src/RegistrantSubmitter.php');
+
+$clientId  = '1111';
+$projectId = '1001';
+$apiKey = '1x1x1';
+
+if (empty($clientId) || empty($projectId) || empty($apiKey)){
+	throw new Exception('Required parameters are not set, please check that
+							your $clientId, $projectId and $apiKey
+							are configured correctly');
+}
+
+$lead = new LassoLead(
+	$_REQUEST['FirstName'],
+    $_REQUEST['LastName'],
+    $projectId,
+    $clientId
+);
+
+$lead->addPhone($_REQUEST['Phone']);
+
+$lead->addEmail($_REQUEST['Email']);
+
+/* Questions (text answer)
+ *
+ * Only for questions that require text input answers
+ * $lead->answerQuestionByIdForText($questionId,$_REQUEST['Questions'][$questionId]);
+ */
+$lead->answerQuestionByIdForText(1111, $_REQUEST['utm_campaign']);
+$lead->answerQuestionByIdForText(2222, $_REQUEST['utm_content']);
+$lead->answerQuestionByIdForText(3333, $_REQUEST['utm_id']);
+$lead->answerQuestionByIdForText(4444, $_REQUEST['utm_medium']);
+$lead->answerQuestionByIdForText(5555, $_REQUEST['utm_source']);
+$lead->answerQuestionByIdForText(6666, $_REQUEST['utm_term']);
+
+$lead->sendAssignmentNotification();
+
+$submitter = new RegistrantSubmitter();
+$curl      = $submitter->submit('https://api.lassocrm.com/registrants', $lead, $apiKey);
